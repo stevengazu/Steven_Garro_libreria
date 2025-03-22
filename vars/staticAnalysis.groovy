e94262a45a6149cc8a37bce6ef6ba4d4b8605160
stevengazu <stevengazu@hotmail.com>
2025-03-22 10:17:10 -0600

    Update staticAnalysis.groovy

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,10 +1,12 @@
-def call(boolean abortPipeline = false) {
+def call(Map params = [:]) {
+    // Extraer parámetros del mapa con valores predeterminados
+    boolean abortPipeline = params.get('abortPipeline', false)
+
     timeout(time: 5, unit: 'MINUTES') {
         // Simulación de análisis estático de código
         sh 'echo "Ejecución de las pruebas de calidad de código"'
     }
-    
-    // Evaluar si se debe abortar el pipeline
+
     if (abortPipeline) {
         error('Abortando el pipeline debido a la configuración de staticAnalysis.')
     }
