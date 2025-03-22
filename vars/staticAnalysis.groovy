1d70cb1bc966c29f02d5d48337a8e09f515c8d42
stevengazu <stevengazu@hotmail.com>
2025-03-22 11:06:21 -0600

    Update staticAnalysis.groovy

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -2,12 +2,22 @@ def call(Map params = [:]) {
     // Extraer parámetros del mapa con valores predeterminados
     boolean abortPipeline = params.get('abortPipeline', false)
 
+    // Leer el nombre de la rama desde las variables de entorno de Jenkins
+    def branchName = env.BRANCH_NAME ?: 'unknown'
+
+    // Lógica para cortar el pipeline según heurística
+    if (abortPipeline) {
+        error('Abortando el pipeline debido a la configuración de staticAnalysis (abortPipeline=true).')
+    } else if (branchName == 'master') {
+        error('Abortando el pipeline porque la rama es "master".')
+    } else if (branchName.startsWith('hotfix')) {
+        error('Abortando el pipeline porque la rama empieza con "hotfix".')
+    } else {
+        echo "La rama es '${branchName}', el pipeline continuará."
+    }
+
     timeout(time: 5, unit: 'MINUTES') {
         // Simulación de análisis estático de código
         sh 'echo "Ejecución de las pruebas de calidad de código"'
-    }
-
-    if (abortPipeline) {
-        error('Abortando el pipeline debido a la configuración de staticAnalysis.')
     }
 }
